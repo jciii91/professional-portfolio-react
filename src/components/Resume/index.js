bfd0b53275fcdcaa7afc00faf5471e9c29d2b7da
JCIII <jcostello711@gmail.com>
2022-06-23 22:20:52 -0400

    resume page looks fine. need to fix mobile view.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,34 +1,44 @@
 import React from 'react';
-import resume from '../../assets/documents/resume.docx'
+import resume from '../../assets/documents/resume.pdf'
+import { Row, Col } from 'react-bootstrap';
 
 function Resume() {
+    const skills = [
+        'JavaScript ES6+',
+        'CSS3',
+        'HTML5',
+        'SQL',
+        'NoSQL',
+        'GitHub',
+        'MongoDB',
+        'MySQL',
+        'GraphQL',
+        'Progressive Web Applications',
+        'Express.js',
+        'React',
+        'Node.js',
+        'Handlebars',
+        'jQuery',
+        'Bootstrap',
+        'Global States',
+        'JSON Web Tokens'
+    ]
     return (
-        <div>
-            <a href={resume}>
-                Click here to download my resume
-            </a>
-            <h3> My Skills </h3>
-            <ul>
-                <li>
-                    Node.js
-                </li>
-                <li>
-                    Express
-                </li>
-                <li>
-                    Handlebars
-                </li>
-                <li>
-                    React
-                </li>
-                <li>
-                    SQL
-                </li>
-                <li>
-                    MongoDB
-                </li>
-            </ul>
-        </div>
+        <>
+            <Row className='text-center mb-4'>
+                <a href={resume} style={{ fontSize: '2rem'}}>Click here to download my resume</a>
+            </Row>
+            <Row className='text-center mb-4'>
+                <h3>Technical Skills</h3>
+            </Row>
+            <Row className='g-2 m-4 justify-content-center'>
+                    {skills.map((skill) => (
+                        <Col xs={3} className='text-center'>
+                            {skill}
+                        </Col>
+                    ))}
+            </Row>
+        </>
     );
 }
 
