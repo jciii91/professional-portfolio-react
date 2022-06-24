53bedc392cafaaf75f34379497391f14bbae0b68
JCIII <jcostello711@gmail.com>
2022-06-23 21:56:30 -0400

    finished portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,41 +6,37 @@ function Portfolio() {
     const projectObjects = [
         {
             name: 'Budget Tracker',
-            description: '',
-            tech: [],
-            sampleGuest: {},
+            description: 'This application is a budget tracker website. It uses IndexedDB for offline functionality, a service worker for interacting with the browser cache, and is also a Progressive Web Application. The website also uses a MongoDB database.',
+            tech: ['Node.js','Express','MongoDB','IndexedDB','PWA'],
             repo: 'https://github.com/jciii91/budget-tracker-PWA',
             deployment: 'https://peaceful-falls-53457.herokuapp.com/'
         },
         {
             name: 'Space Launch',
-            description: '',
-            tech: [],
-            sampleGuest: {},
+            description: 'As space travel enthusiasts we created an application that tracks space launches. The user has two formats of launch selections: date selection, or a list of the next 10 launches.',
+            tech: ['HTML', 'CSS', 'Javascript', 'Materialize', 'jQuery', 'Mapquest API', 'The SpaceDevs API'],
             repo: 'https://github.com/jciii91/space-launch',
             deployment: 'https://jciii91.github.io/space-launch/'
         },
         {
-            name: 'Upscale Delivery',
-            description: '',
-            tech: [],
-            sampleGuest: {},
-            repo: 'https://github.com/FernandoMoreno1/Project-2',
-            deployment: 'https://upscale-delivery.herokuapp.com/'
+            name: 'Book Search Engine',
+            description: 'This project uses the full MERN stack to create a search engine for books using Googles API. Users can create accounts, log in and out, search for books and save books they find to be viewed later. Any user can search but you must be signed in to save books.',
+            tech: ['Apollo', 'GraphQL', 'Express', 'React', 'MongoDB', 'Google Books API'],
+            sampleGuest: {username: 'guest', email: 'guest@guest.guest', password: 'password123'},
+            repo: 'https://github.com/jciii91/book-search-engine',
+            deployment: 'https://young-everglades-13808.herokuapp.com/'
         },
         {
-            name: 'Note Taker',
-            description: '',
-            tech: [],
-            sampleGuest: {},
-            repo: 'https://github.com/jciii91/note-taker',
-            deployment: 'https://mysterious-spire-35262.herokuapp.com/'
+            name: 'Work Day Scheduler',
+            description: 'A simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.',
+            tech: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Moment.js'],
+            repo: 'https://github.com/jciii91/workday-scheduler',
+            deployment: 'https://jciii91.github.io/workday-scheduler/'
         },
         {
             name: 'Weather Dashboard',
-            description: '',
-            tech: [],
-            sampleGuest: {},
+            description: 'Uses the OpenWeather One Call API to retrieve weather data for cities. Weather Dashboard will show current information for the searched city as well as the 5 day forecast. There is also a search history which persists between pageloads. Clicking on a previously searched city will pull its current weather data and its 5 day forecast.',
+            tech: ['HTML', 'CSS', 'JavaScript', 'Local Storage', 'OpenWeather One Call API'],
             repo: 'https://github.com/jciii91/weather-dashboard',
             deployment: 'https://jciii91.github.io/weather-dashboard/'
         }
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 import project0 from "../../assets/img/0.png"
 import project1 from "../../assets/img/1.png"
 import project2 from "../../assets/img/2.png"
@@ -7,18 +7,34 @@ import project3 from "../../assets/img/3.png"
 import project4 from "../../assets/img/4.png"
 
 function Project({project, i}) {
-    const {name, repo, deployment} = project;
+    const {name, repo, deployment, description, tech, sampleGuest} = project;
     const projectImgs = [project0, project1, project2, project3, project4];
     return (
-        <Card style={{ width: '20rem' }}>
+        <Card style={{ width: '30rem' }}>
             <Card.Img variant='top' src={projectImgs[i]} alt='Project Image' />
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
                 <Card.Text>
-
+                    {description}
                 </Card.Text>
-                <Button variant='primary' href={repo} target='_blank'>Repository</Button>
-                <Button variant='secondary' href={deployment} target='_blank'>Deployment</Button>
+                <Card.Title>Technologies Used</Card.Title>
+                <ListGroup>
+                    {tech.map((item) => (
+                        <ListGroupItem className='border-0'>• {item}</ListGroupItem>
+                    ))}
+                </ListGroup>
+                {sampleGuest &&
+                <>
+                    <Card.Title>Guest Account Credentials</Card.Title>
+                    <ListGroup>
+                        <ListGroupItem className='border-0'>• Username: {sampleGuest.username}</ListGroupItem>
+                        <ListGroupItem className='border-0'>• Email: {sampleGuest.email}</ListGroupItem>
+                        <ListGroupItem className='border-0'>• Password: {sampleGuest.password}</ListGroupItem>
+                    </ListGroup>
+                </>
+                }
+                <Button className='mt-3' variant='primary' href={repo} target='_blank'>Repository</Button>
+                <Button className='mt-3 mx-3' variant='secondary' href={deployment} target='_blank'>Deployment</Button>
             </Card.Body>
         </Card>
     );
